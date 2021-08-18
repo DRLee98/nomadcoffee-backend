09bdf6c0ee290e85296deb78c88f68af0ff6a907
DRLee98 <64233118+DRLee98@users.noreply.github.com>
2021-08-18 20:11:31 +0900

    deleteCoffeeShop

---
+++ src/coffeeShop/deleteCopffeeShop/deleteCopffeeShop.resolvers.js
@@ -0,0 +1,41 @@
+import client from "../../client";
+import { protectResolver } from "../../users/users.utils";
+import { getCategoryObj, getPhotoObj } from "../coffeeShop.utils";
+
+export default {
+  Mutation: {
+    deleteCopffeeShop: protectResolver(async (_, { id }, { loggedInUser }) => {
+      try {
+        const coffeeShop = await client.coffeeShop.findUnique({
+          where: { id },
+        });
+        if (!coffeeShop) {
+          return {
+            ok: false,
+            error: "Coffee Shop not found.",
+          };
+        }
+        if (coffeeShop.userId !== loggedInUser.id) {
+          return {
+            ok: false,
+            error: "Not authorized.",
+          };
+        }
+        await client.coffeeShop.delete({
+          where: {
+            id,
+          },
+        });
+        return {
+          ok: true,
+        };
+      } catch (error) {
+        console.log(error);
+        return {
+          ok: false,
+          error,
+        };
+      }
+    }),
+  },
+};
