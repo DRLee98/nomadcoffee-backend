a665d5409f76c533036f7b7ea5eb24d3ad72ff6d
DRLee98 <64233118+DRLee98@users.noreply.github.com>
2021-09-02 17:23:25 +0900

    token error

--- src/users/users.utils.js
+++ src/users/users.utils.js
@@ -2,20 +2,25 @@ import jwt from "jsonwebtoken";
 import client from "../client";
 
 export const getUser = async (token) => {
-  if (!token || token === null) {
-    return null;
-  }
-  const verifiedToken = await jwt.verify(token, process.env.PRIVATE_KEY);
-  if (verifiedToken.id) {
-    const user = await client.user.findUnique({
-      where: { id: verifiedToken.id },
-    });
-    if (user) {
-      return user;
+  try {
+    if (!token || token === null) {
+      return null;
+    }
+    const verifiedToken = await jwt.verify(token, process.env.PRIVATE_KEY);
+    if (verifiedToken.id) {
+      const user = await client.user.findUnique({
+        where: { id: verifiedToken.id },
+      });
+      if (user) {
+        return user;
+      }
+      return null;
     }
     return null;
+  } catch (error) {
+    console.log(error);
+    return null;
   }
-  return null;
 };
 
 export const protectResolver = (resolver) => (parent, args, context, info) => {
