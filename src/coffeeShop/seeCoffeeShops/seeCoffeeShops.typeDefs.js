331e043fa93a68c15a64a8eafcd2df0a6471a28c
DRLee98 <64233118+DRLee98@users.noreply.github.com>
2021-09-06 17:22:06 +0900

    seeCoffeeShops 수정

--- src/coffeeShop/seeCoffeeShops/seeCoffeeShops.typeDefs.js
+++ src/coffeeShop/seeCoffeeShops/seeCoffeeShops.typeDefs.js
@@ -1,12 +1,7 @@
 import { gql } from "apollo-server-express";
 
 export default gql`
-  type seeCoffeeShopsResult {
-    totalPage: Int!
-    totalCount: Int!
-    shops: [CoffeeShop!]
-  }
   type Query {
-    seeCoffeeShops(page: Int): seeCoffeeShopsResult
+    seeCoffeeShops(page: Int): [CoffeeShop!]
   }
 `;
